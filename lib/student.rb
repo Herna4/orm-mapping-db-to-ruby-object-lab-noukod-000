4cb7c16fb4dfa5c3d94b5fe51a030dd3fa42f6b0
Herna Michel <labellehernamichel@yahoo.fr>
2019-05-19 16:53:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -61,16 +61,16 @@ class Student
   end
 
    def self.first_X_students_in_grade_10(num)
-      sql = <<-SQL
-        SELECT * FROM students
-        WHERE grade = 10
-        LIMIT ?
-      SQL
+    sql = <<-SQL
+     SELECT * FROM students
+     WHERE grade = 10
+     LIMIT ?
+    SQL
   
-      DB[:conn].execute(sql, num).map do |row|
+    DB[:conn].execute(sql, num).map do |row|
         self.new_from_db(row)
-      end
-  end
+    end
+   end
   
   def self.first_student_in_grade_10
     sql = <<-SQL
