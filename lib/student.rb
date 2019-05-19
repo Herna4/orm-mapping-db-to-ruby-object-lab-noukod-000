696aa98c02c19365b0addd73c8f6ea36193c8991
Herna Michel <labellehernamichel@yahoo.fr>
2019-05-19 16:52:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -84,17 +84,6 @@ class Student
       self.new_from_db(row)
     end.first
   end
-
-  # def self.all_students_in_grade_x(grade)
-  #   # sql = <<-SQL
-  #   #   SELECT * FROM students
-  #   #   WHERE grade = ?
-  #   #   SQL
-
-  #   #   DB[:conn].execute(sql, grade).map do |row|
-  #   #     self.new_from_db(row)
-   
-  # end
   
   def self.all_students_in_grade_X(grade)
 
@@ -102,6 +91,7 @@ class Student
         SELECT * From students 
         where grade = ?
       SQL
+      
       DB[:conn].execute(sql, grade).map do |row|
        self.new_from_db(row)
      end
